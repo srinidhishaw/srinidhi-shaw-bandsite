0a319205c70a8b5f81f12cf48036552ddca25203
Srinidhi Shaw <srinidhishaw@gmail.com>
2023-10-15 00:28:52 -0400

    add api and retrieve comments from api. set up delete and post functionality. adjust pre-existing code to use api to display comments

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,3 +1,130 @@
-const APIURL ="https://project-1-api.herokuapp.com/register";
 
+// ?api_key=<your_api_key_here>
 // {"api_key":"03ae98ad-15f5-4aae-b471-58102c9276f5"}
+// const apiUrl ="https://project-1-api.herokuapp.com/comments?api_key=03ae98ad-15f5-4aae-b471-58102c9276f5";
+
+// async function fetchComments(){
+//     try{
+//         const commentArr = await axios.get(apiUrl);
+//     console.log(commentArr.data);
+//     }
+//     catch(error){
+//         console.log("Error");
+//     }
+    
+// }
+
+// fetchComments();
+
+class BandSiteAPI{
+    constructor(apiKey){
+        this.apiKey = "b1698d1c-8ba4-4a9e-beb7-0a5e27348a39";
+        this.baseUrl = "https://project-1-api.herokuapp.com/";
+    }
+
+    async postComment(com){
+        try{
+        const response = await fetch(this.baseUrl+"comments"+"?api_key="+this.apiKey, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(com),
+        });
+    
+        return await response.json();
+    }
+    catch{
+        console.error();
+    }        
+
+    }
+
+    async deleteComment(id){
+        try{
+            const deleteCom = await axios.delete(this.baseUrl+"comments/"+id+"?api_key="+this.apiKey);
+        return deleteCom.json
+        }
+        catch{
+            console.error();
+        }
+        
+    }
+    // const onPageLoad = async () => {
+    //     const response = await fetch(`/api/project/projects`, {
+    //         method: 'GET',
+    //         headers: { 'Content-Type': 'application/json' },
+    //     })
+        
+    //     if (response.ok) {
+    //         console.log(response.json())
+    
+    //     } else {
+    //         console.error(err)
+    //     }
+    // }
+    
+    // onPageLoad();
+
+    async getComments(){
+        try{
+            
+            const commentArr = await axios.get(this.baseUrl+"comments"+"?api_key="+this.apiKey);
+                return(commentArr);
+        }
+        catch{
+            console.error();
+        }
+
+    }
+    
+    async getShowDates(){
+        try{
+            const showDateArr = await axios.get(this.baseUrl+"showdates"+"?api_key="+this.apiKey);
+            // const date = new Date(timestamp);
+            console.log(showDateArr.data);
+        }
+        catch(error){
+            console.error();
+        }
+    }
+
+   async getShows(){
+        try{
+            const showsArr = await axios.get(this.baseUrl+"shows"+"?api_key="+this.apiKey);
+            console.log(showsArr.data);
+            
+        }
+        catch(error){
+            console.error();
+        }
+
+    }
+}
+const seeShows = new BandSiteAPI()
+
+// console.log(seeShows.getShowDates())
+// console.log(seeShows.getShows())
+const newcom = 
+{
+    "name": "Nigel",
+    "comment": "What a cool site"
+};
+
+// console.log(seeShows.deleteComment("e7e8ef59-c27c-42e9-9de3-f99fc99256d2"))
+// console.log(seeShows.deleteComment("48a64df0-46ca-48d9-b547-94467d799a56"))
+// console.log(seeShows.deleteComment("473179f2-1afa-4608-84ac-a71dc43346c5"))
+// console.log(seeShows.deleteComment("ed28620b-6fc3-49b5-b168-32dbb3573f8a"))
+// console.log(seeShows.deleteComment("81f0f615-223e-468f-86b3-5b67dfd1b0d7"))
+// console.log(seeShows.deleteComment("6326432a-4804-49cc-a73c-e6870be0594b"))
+// console.log(seeShows.deleteComment("65a6e690-e34a-4ce5-b669-fd4035209e8d"))
+// console.log(seeShows.deleteComment("7b70b012-9b7c-413f-934b-8a7c6d7158bf"))
+// console.log(seeShows.deleteComment("5b5580cd-cea6-49ea-b10b-62296e913391"))
+// console.log(seeShows.deleteComment("58ea0c9a-ee5c-44c7-bcff-85b0ccc2c371"))
+// console.log(seeShows.deleteComment("8e8934cf-3566-4560-9f06-876061cca431"))
+// console.log(seeShows.deleteComment("897392fa-49a1-42e8-9366-7620b54822ca"))
+// console.log(seeShows.deleteComment("bf25c665-eac7-49e9-8b58-8a9924cdaa4b"))
+
+console.log(seeShows.getComments())
+
+export default BandSiteAPI;
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,153 +1,279 @@
-const commentsArr = [
-    {
-        name:"Connor Walton",
-        timeStamp:"02/17/2021",
-        comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
-    {
-        name:"Emilie Beach",
-        timeStamp:"01/09/2021",
-        comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        name:"Miles Acosta",
-        timeStamp:"12/20/2020",
-        comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    },
+import BandSiteAPI from './band-site-api.js';
+
+let apiKey="b1698d1c-8ba4-4a9e-beb7-0a5e27348a39";
+const seeShows = new BandSiteAPI(apiKey);
+let commentsArr=[];
+// const commentsArr = [
+//     {
+//         name:"Connor Walton",
+//         timeStamp:"02/17/2021",
+//         comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+//     },
+//     {
+//         name:"Emilie Beach",
+//         timeStamp:"01/09/2021",
+//         comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name:"Miles Acosta",
+//         timeStamp:"12/20/2020",
+//         comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+//     },
     
-]
-const titleArr = {
-  name:"Name: ", comment:"Comment: "
-}
-const comments = document.createElement("section");
+// ]
+// console.log(commentArray);
 
-const formContainer = document.createElement("section");
-
-const getStartedTitle = document.createElement('h1');
-getStartedTitle.innerText="Join the Conversation";
-
-getStartedTitle.classList.add("getstarted-title");
-const commentForm = document.createElement('form');
-const userImg = document.createElement("img");
-const inputName = document.createElement("input");
-const inputCom= document.createElement("input");
-const inputButton = document.createElement("input");
-const card = document.createElement("div")
-card.classList.add("card");
-const titleName = document.createElement("p")
-const titleComment=document.createElement("p")
-titleName.innerText=titleArr.name;
-titleComment.innerText= titleArr.comment;
-titleName.classList.add("titles")
-titleComment.classList.add("titles")
-
-inputName.type = "text";
-inputCom.type = "text";
-inputName.name = "name";
-inputCom.name = "comment";
-userImg.setAttribute("src","../assets/Images/Mohan-muruge.jpg");
-
-userImg.classList.add("user-image");
-inputName.placeholder = "Enter your name";
-inputName.classList.add("input")
-inputCom.classList.add("input")
-inputCom.placeholder = "Add a new comment";
-inputCom.style.height="100px";
-inputButton.type = "submit";
-inputButton.value = "COMMENT";
-inputButton.classList.add("input-button");
-commentForm.appendChild(userImg);
-card.appendChild(titleName)
-card.appendChild(inputName);
-card.appendChild(titleComment)
-card.appendChild(inputCom);
-card.appendChild(inputButton);
-
-commentForm.appendChild(card);
-formContainer.appendChild(commentForm);
-document.querySelector("main").appendChild(getStartedTitle);
-document.querySelector("main").appendChild(formContainer);
-commentForm.classList.add("comment-form");
-formContainer.classList.add("form-container");
-const submissionsArray = [];
-
-commentForm.addEventListener("submit", function (event) {
-    
-  event.preventDefault();
-if(inputName.value !== ""){
-  const name = inputName.value;
-    const comment = inputCom.value;
-    const date = new Date();
-    const timeStamp = date.getDate() + "/"+(date.getMonth()+1)  + "/"+date.getFullYear();
-    // + " @ "  
-    // + date.getHours() + ":"  
-    // + date.getMinutes() + ":" 
-    // + date.getSeconds();
-
- console.log(timeStamp);
-  const submissionData = {
-    name,
-    timeStamp,
-    comment
-    
-  };
-
-   commentsArr.splice(0,0,submissionData);
-   comments.innerText = "";
-  for(i=0;i<3;i++){
-    displayComment(commentsArr[i]);
-  }
-  
-  commentForm.reset();
-}
-});
-
-
-comments.innerText = "";
-for (let i=0;i<commentsArr.length;i++){
-function displayComment(commentObj){
-        // console.log("hi")
-        const commentCard = document.createElement("article");
-        const commentHeader1=document.createElement("div")
-        const commentHeader2=document.createElement("div")
-        const commentName = document.createElement("p");
-        const commentTime = document.createElement("p");
-        const commentText = document.createElement("p");
-        const colorPlaceholder = document.createElement("div");
-        const extraDiv=document.createElement("div");
-        colorPlaceholder.style.backgroundColor = "#E1E1E1";
+async function importComments(){
+  try {
+    commentsArr=[]
+      const commentPromise = await seeShows.getComments();
+      
+      const commentArr = commentPromise.data;
+      console.log(commentArr)
+      commentArr.forEach(key => {
+        const date = new Date(key.timestamp);
+        const time = date.getDate() + "/"+(date.getMonth()+1)  + "/"+date.getFullYear();
+        const comObj={
+          name:key.name,
+          timeStamp: time,
+          comment: key.comment,
+          id:key.id,
+          likes:key.likes
+        }
+        commentsArr.push(comObj);
         
-        commentCard.classList.add("comment-card");
-        colorPlaceholder.classList.add("comment-image");
         
-        commentName.innerText = commentObj.name;
-        commentTime.innerText = commentObj.timeStamp;
-        commentText.innerText = commentObj.comment;
-        
-        commentName.classList.add("comment-styles");
-        commentText.classList.add("comment-styles")
-
-        commentName.style.fontSize='$mobile-bodycopy--size' ;
-        commentName.style.fontFamily='AvenirNextBold';
-        commentName.style.color='$navbarcolor';
-        commentCard.appendChild(colorPlaceholder);
-        commentHeader1.appendChild(commentName);
-        commentHeader1.appendChild(commentTime);
-        commentHeader1.classList.add("header-style");
-        
-        commentCard.appendChild(commentHeader1);
-        commentCard.appendChild(commentText);
-        extraDiv.appendChild(colorPlaceholder);
-        extraDiv.appendChild(commentCard)
-        comments.appendChild(extraDiv);
-        extraDiv.classList.add("extra-div")
-        comments.classList.add("comments");
-        document.querySelector("main").appendChild(comments);
-        
+    });
+    for(let i=commentsArr.length-1;i>=0;i--){
+      displayComment(commentsArr[i]);
     }
     
-displayComment(commentsArr[i]);
+  } catch (error) {
+    console.error(error);
 }
+}
+importComments();
+    
+
+  const titleArr = {
+    name:"Name: ", comment:"Comment: "
+  }
+  const comments = document.createElement("section");
+  
+  const formContainer = document.createElement("section");
+  
+  const getStartedTitle = document.createElement('h1');
+  getStartedTitle.innerText="Join the Conversation";
+  
+  getStartedTitle.classList.add("getstarted-title");
+  const commentForm = document.createElement('form');
+  const userImg = document.createElement("img");
+  const inputName = document.createElement("input");
+  const inputCom= document.createElement("input");
+  const inputButton = document.createElement("input");
+  const card = document.createElement("div")
+  card.classList.add("card");
+  const titleName = document.createElement("p")
+  const titleComment=document.createElement("p")
+  titleName.innerText=titleArr.name;
+  titleComment.innerText= titleArr.comment;
+  titleName.classList.add("titles")
+  titleComment.classList.add("titles")
+  
+  inputName.type = "text";
+  inputCom.type = "text";
+  inputName.name = "name";
+  inputCom.name = "comment";
+  userImg.setAttribute("src","../assets/Images/Mohan-muruge.jpg");
+  
+  userImg.classList.add("user-image");
+  inputName.placeholder = "Enter your name";
+  inputName.classList.add("input")
+  inputCom.classList.add("input")
+  inputCom.placeholder = "Add a new comment";
+  inputCom.style.height="100px";
+  inputButton.type = "submit";
+  inputButton.value = "COMMENT";
+  inputButton.classList.add("input-button");
+  commentForm.appendChild(userImg);
+  card.appendChild(titleName)
+  card.appendChild(inputName);
+  card.appendChild(titleComment)
+  card.appendChild(inputCom);
+  card.appendChild(inputButton);
+  
+  commentForm.appendChild(card);
+  formContainer.appendChild(commentForm);
+  document.querySelector("main").appendChild(getStartedTitle);
+  document.querySelector("main").appendChild(formContainer);
+  commentForm.classList.add("comment-form");
+  formContainer.classList.add("form-container");
+
+
+
+    const submissionsArray = [];
+    let showCom = false;
+async function submitEvent(event){
+      event.preventDefault();
+      
+      if(inputName.value !== ""){
+        
+        const name = inputName.value;
+          const comment = inputCom.value;
+          const date = new Date();
+          const timeStamp = date.getDate() + "/"+(date.getMonth()+1)  + "/"+date.getFullYear();
+          // + " @ "  
+          // + date.getHours() + ":"  
+          // + date.getMinutes() + ":" 
+          // + date.getSeconds();
+  
+        const submissionData = {
+          name,
+          // timeStamp,
+          comment
+          
+        };
+        const postData = await seeShows.postComment(submissionData);
+        console.log(postData)
+        comments.innerText = "";
+        importComments();
+        
+        
+        
+        commentForm.reset();
+      }
+      
+    }
+    commentForm.addEventListener("submit",submitEvent);
+    async function delComment(event) {
+      // Check if the clicked element is a delete button
+      if (event.target.classList.contains('del-button')) {
+
+        event.preventDefault();
+        const comId = event.target.id;
+          console.log("hi")
+          const deleteId = await seeShows.deleteComment(comId);
+        // console.log(postData)
+        comments.innerText = "";
+        importComments();
+      }}
+
+      document.addEventListener('click',delComment)
+    // async function delEvent(event){
+    //   console.log("hi")
+    //   event.preventDefault();
+    //   const comId = event.target.id;
+    //   console.log(comId);
+    // }
+    // document.querySelectorAll("del-button").addEventListener("click",delEvent);
+    
+comments.innerText = "";
+
+async  function displayComment(commentObj){
+            console.log("hi")
+            const commentCard = document.createElement("article");
+            const commentHeader1=document.createElement("div")
+            const commentHeader2=document.createElement("div")
+            const commentName = document.createElement("p");
+            const commentTime = document.createElement("p");
+            const commentText = document.createElement("p");
+            const delButton = document.createElement("button");
+            delButton.textContent="Delete"
+            delButton.id=`${commentObj.id}`
+            console.log(delButton.id)
+            delButton.classList.add("del-button")
+            delButton.classList.add("buttons");
+            const likeButton = document.createElement("button");
+            likeButton.classList.add("buttons");
+            likeButton.textContent="Like"
+            const buttonDiv = document.createElement("div");
+            buttonDiv.classList.add("button-div");
+            buttonDiv.appendChild(delButton);
+            buttonDiv.append(likeButton)
+            const colorPlaceholder = document.createElement("div");
+            const extraDiv=document.createElement("div");
+            colorPlaceholder.style.backgroundColor = "#E1E1E1";
+            
+            commentCard.classList.add("comment-card");
+            colorPlaceholder.classList.add("comment-image");
+            
+            commentName.innerText = commentObj.name;
+            commentTime.innerText = commentObj.timeStamp;
+            commentText.innerText = commentObj.comment;
+            
+            commentName.classList.add("comment-styles");
+            commentText.classList.add("comment-styles")
+
+            commentName.style.fontSize='$mobile-bodycopy--size' ;
+            commentName.style.fontFamily='AvenirNextBold';
+            commentName.style.color='$navbarcolor';
+            commentCard.appendChild(colorPlaceholder);
+            commentHeader1.appendChild(commentName);
+            commentHeader1.appendChild(commentTime);
+            commentHeader1.classList.add("header-style");
+            
+            commentCard.appendChild(commentHeader1);
+            commentCard.appendChild(commentText);
+            
+            extraDiv.appendChild(colorPlaceholder);
+            extraDiv.appendChild(commentCard)
+            comments.appendChild(extraDiv);
+            extraDiv.classList.add("extra-div")
+            commentCard.appendChild(buttonDiv);
+            // extraDiv.appendChild(likeButton);
+            comments.classList.add("comments");
+            document.querySelector("main").appendChild(comments);
+            
+        }
+        for (let i=commentsArr.length-1;i>=0;i--){
+          // console.log("hi")
+          displayComment(commentsArr[i]);
+          }        
+    
+  
+// commentForm.addEventListener("submit", submitEvent);
+    
+  
+
+const band = new BandSiteAPI();
+
+
+// console.log(commentsArr)
+    
+    
+    
+    
+    
+    // window.globalFunction = displayComment;
+
+// const commentsArr = [
+//     {
+//         name:"Connor Walton",
+//         timeStamp:"02/17/2021",
+//         comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+//     },
+//     {
+//         name:"Emilie Beach",
+//         timeStamp:"01/09/2021",
+//         comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name:"Miles Acosta",
+//         timeStamp:"12/20/2020",
+//         comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+//     },
+    
+// ]
+
+
+
+
+// commentForm.addEventListener("submit", function (event) {
+    
+//   event.preventDefault();
+// }
 
 // console.log(comments);
  
+
+
